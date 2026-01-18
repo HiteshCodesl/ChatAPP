f73b43beb6f3d369471076da2e323e35126a3b0c
Hitesh Navare <navarehitesh65@gmail.com>
2026-01-18 17:52:24 +0530

    added get rooms and chats from the users

--- Backend/src/routes/roomRoute.ts
+++ Backend/src/routes/roomRoute.ts
@@ -1,6 +1,6 @@
 import express from "express"
 import { authMiddleware } from "../middleware/auth.js";
-import { roomModel } from "../db/db.js";
+import { chatModel, roomModel } from "../db/db.js";
 import { createRoomSchema } from "../utils/roomTypes.js";
 
 export const roomRouter = express.Router();
@@ -52,4 +52,44 @@ roomRouter.post('/create', authMiddleware, async (req, res) => {
     })
 })
 
+roomRouter.get('/getRooms', authMiddleware, async(req, res) => {
+    const userId = req.id;
 
+    const rooms = await roomModel.find({
+        users: userId
+    }).select("_id roomName")
+
+    console.log("rooms", rooms);
+     if(!rooms){
+        return res.status(401).json({
+            "success": "false",
+            "error": "failed to find the room or chat"
+        })
+    } 
+
+    return res.status(201).json({
+        "success": "true",
+        "data": rooms
+    })
+})
+
+roomRouter.get('/chats/:id', authMiddleware, async (req, res) => {
+    const roomId = req.params.id as string;
+    console.log("room", roomId)
+
+    const messages = await chatModel.find({
+        room: roomId
+    }).populate("user", "name, email").limit(100);
+    
+    if(!messages){
+        return res.status(401).json({
+            "success": "false",
+            "error": "failed to find the room or chat"
+        })
+    }
+
+    return res.status(201).json({
+        "success": "true",
+        "data": messages
+    })
+})
