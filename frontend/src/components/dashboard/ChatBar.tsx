1dadcb533d3883d012135884bc9db16ab40cbb6d
Hitesh Navare <navarehitesh65@gmail.com>
2026-01-21 16:34:30 +0530

    added the chatbar ui

--- frontend/src/components/dashboard/ChatBar.tsx
+++ frontend/src/components/dashboard/ChatBar.tsx
@@ -5,8 +5,9 @@ import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { Send } from "lucide-react";
 import { Badge } from "../ui/badge";
+import { useSocket } from "../../config/socket";
 
-interface ChatInterface{
+interface ChatInterface {
   _id: string;
   message: string;
   user: UserInterface;
@@ -20,41 +21,82 @@ interface UserInterface {
 
 export default function ChatBar() {
   const [chats, setChats] = useState<ChatInterface[]>([]);
-  const {roomId, isSelected } = useRoom();
-console.log("roomId", roomId);
+  const { roomId, isSelected, roomName } = useRoom();
+  const [message, setMessage] = useState('');
+  const { ws } = useSocket();
 
   useEffect(() => {
-    const fetchChats = async() => {
+    const fetchChats = async () => {
       console.log("fetchChat activated")
-     const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/room/chats/${roomId}`, {
-      headers: {
-        "Authorization": localStorage.getItem('token')
+      const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/room/chats/${roomId}`, {
+        headers: {
+          "Authorization": localStorage.getItem('token')
+        }
+      });
+      console.log("response", response);
+
+      if (response.status === 201) {
+        const data = response.data.data;
+        console.log(data);
+        setChats(data);
       }
-     });
-     console.log("response",response);
-     
-     if(response.status === 201){
-      const data = response.data.data;  
-      console.log(data);
-      setChats(data);
-     }
-  }
-  console.log("calling a function");
-  fetchChats();
+    }
+    console.log("calling a function");
+    fetchChats();
   }, [isSelected, roomId])
-  
+
+
+  useEffect(() => {
+    if (!ws) return;
+
+    const handler = (msg: MessageEvent) => {
+
+      const parsed = JSON.parse(msg.data);
+      console.log("parsed", parsed);
+
+      if (parsed.type === "CHAT") {
+        setChats((prev) => [...prev, parsed.payload]);
+      }
+    }
+    ws.addEventListener("message", handler);
+    return () => {
+      ws.removeEventListener("message", handler);
+    };
+  }, [ws])
+
+
+
+  const sendMessage = async () => {
+    if (!message || !ws) return;
+    console.log("message", message)
+    ws?.send(JSON.stringify({
+      "type": "CHAT",
+      "message": message
+    }))
+    setMessage("");
+  }
+
   return (
-    <div className="w-[50vw] border-r border-gray-600 text-xl text-white mt-5 mx-5 flex flex-col gap-2 h-screen ">
+    <div className=" w-screen text-xl text-white">
+
+
+      <div className="bg-zinc-700 py-5 px-8 text-xl text-white">
+        {roomName}
+      </div>
+
+      <div className=" mt-5  flex flex-col gap-2 h-screen ">
         {chats.map((chat) => (
-          <div key={chat._id}>
-               <Badge variant={'default'}>{chat.user.name}</Badge>
-              <p className="p-2 border-white flex text-md text-white">{chat.message}</p>
+          <div key={chat._id} className="mx-5">
+            <Badge variant={'default'}>{chat.user.name}</Badge>
+            <p className="p-2 border-white flex text-md font-semibold text-white">{chat.message}</p>
           </div>
         ))}
-        <div className="flex gap-2 items-center bottom-10 fixed" >
-        <Input  className="w-[45vw] border-r-2" />
-        <Button className="bg-[#7c3aed] hover:bg-[#6d28d9] px-3.5 py-5  items-center rounded-full"><Send /></Button>
+        <div className="flex items-center bottom-0 fixed gap-4 bg-zinc-700 p-6" >
+          <Input value={message} onChange={(e) => setMessage(e.target.value)} className="w-[81vw] py-5 border-r-2" />
+          <Button onClick={sendMessage} className="bg-zinc-600 hover:bg-[#6d28d9] px-4 py-6 items-center rounded-full"><Send /></Button>
         </div>
+      </div>
+
     </div>
   )
 }
