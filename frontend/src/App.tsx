8da56e4814588ef9a5865a517b868abf22882525
Hitesh Navare <navarehitesh65@gmail.com>
2026-01-22 16:28:17 +0530

    added the chatroom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { LandingPage } from './components/dashboard/LandingPage';
 import { SignupPage } from './components/dashboard/SignupPage';
 import { LoginPage } from './components/dashboard/LoginPage';
+import ChatRoom from './components/dashboard/ChatRoom';
+import AppLayout from './components/dashboard/AppLayout';
 
 
 function App() {
@@ -12,7 +14,12 @@ function App() {
       <Routes>
         <Route path='/' element={<LandingPage />} />
         <Route path='/signup' element={<SignupPage />} />
-        <Route path='login' element={<LoginPage />} /> 
+        <Route path='/login' element={<LoginPage />} /> 
+        <Route path='/room/:roomId' element={
+        <AppLayout>
+          <ChatRoom />
+        </AppLayout>
+        } />
       </Routes>
       </BrowserRouter>
      
