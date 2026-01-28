791e671b71f44cf3361123cdcb9065495759663a
Hitesh Navare <navarehitesh65@gmail.com>
2026-01-28 14:14:13 +0530

    modified profile Dialog

--- frontend/src/components/dashboard/ProfileDialog.tsx
+++ frontend/src/components/dashboard/ProfileDialog.tsx
@@ -11,7 +11,7 @@ import axios from "axios";
 import { Button } from "../ui/button";
 import { UserPen } from "lucide-react";
 import type { UserInterface } from "./ChatBar";
-
+import { toast } from "sonner";
 
 interface dialogInterface {
     isOpen: boolean;
@@ -22,51 +22,87 @@ interface dialogInterface {
 export function ProfileDialog({ isOpen, setIsOpen, userDetails }: dialogInterface) {
     const [file, setFile] = useState();
     const [profilePic, setProfilePic] = useState();
+    const [name, setName] = useState("");
+    const [oldPassword, setOldPassword] = useState("");
+    const [newPassword, setNewPassword] = useState("");
 
-        const handleUploadProfilePic = async () => {
-            if (!file) {
-                console.log("image not imported");
-                return;
-            }
-            console.log("inside handleUpload");
-            const formData = new FormData();
-            formData.append('image', file);
-
-            const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/user/upload`, formData, {
-                headers: {
-                    "Authorization": localStorage.getItem("token")
-                }
-            })
-
-            const data = response.data.imageUrl;
-            setProfilePic(data);
+    const handleUploadProfilePic = async () => {
+        if (!file) {
+            console.log("image not imported");
+            return;
         }
+        console.log("inside handleUpload");
+        const formData = new FormData();
+        formData.append('image', file);
 
+        const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/user/upload`, formData, {
+            headers: {
+                "Authorization": localStorage.getItem("token")
+            }
+        })
+        const data = response.data.imageUrl;
+        setProfilePic(data);
+    }
+
+    const changeName = async () => {
+        const response = await axios.patch(`${import.meta.env.VITE_BACKEND_URL}/user/update`, { name }, {
+            headers: {
+                "Authorization": localStorage.getItem("token")
+            }
+        })
+
+        if (response.status === 201) {
+            toast.success(response.data.data);
+        } else {
+            toast.error(response.data.error)
+        }
+        setName('');
+    }
+
+    const changePassword = async () => {
+        const response = await axios.patch(`${import.meta.env.VITE_BACKEND_URL}/user/update`, { password: { oldPassword, newPassword } }, {
+            headers: {
+                "Authorization": localStorage.getItem("token")
+            }
+        })
+
+        if (response.status === 201) {
+            toast.success(response.data.data);
+        } else {
+            toast.error(response.data.error);
+        }
+        setNewPassword('');
+        setOldPassword('');
+    }
 
     return (
         <Dialog open={isOpen} onOpenChange={setIsOpen}>
             <DialogContent>
                 <DialogHeader>
-                    <DialogTitle className="text-center mx-auto flex items-center gap-2 border p-2 bg-[#818CF8] rounded-sm text-black">Update Profile<UserPen /></DialogTitle>
+                    <DialogTitle className="text-center mx-auto flex items-center gap-2 mb-2 p-2 rounded-sm text-black">Update Profile<UserPen /></DialogTitle>
                     <DialogDescription >
+                        <p className="font-semibold text-center p-12">UserId - {userDetails?._id}</p>
                         <div className="flex items-center gap-12">
-                           <div className="flex flex-col gap-2">
-                            <img className="px-3 py-4 rounded-full w-28" src={userDetails?.profileUrl} />
-                            <Input onChange={(e) => setFile(e.target.files[0])} accept="image/*" type="file" className=" border-dashed border-black rounded-full w-32 bottom-2" placeholder="Change Profie Photo" title="Change Profie Photo" /> 
-                            <Button onClick={handleUploadProfilePic}>Change Profile</Button>   
-                           </div>  
-                       
-                        <div className="flex  flex-col gap-8">
-                            <div className="flex flex-col gap-1">
-                                <h1>Change Name</h1>
-                                <Input placeholder={userDetails?.name} />
+                            <div className="flex flex-col gap-2">
+                                <img className="px-3 py-4 rounded-full w-28" src={userDetails?.profileUrl} />
+                                <Input onChange={(e) => setFile(e.target.files[0])} accept="image/*" type="file" className=" border-dashed border-black rounded-full w-32 bottom-2" placeholder="Change Profie Photo" title="Change Profie Photo" />
+                                <Button onClick={handleUploadProfilePic}>Change Profile</Button>
                             </div>
-                            <div className="flex flex-col gap-1">
-                                <h1>Change Password</h1>
-                                <Input placeholder="Enter Current Password" />
+
+                            <div className="flex  flex-col gap-8">
+                                <div className="flex flex-col gap-2">
+                                    <h1>Change Name</h1>
+                                    <Input placeholder={userDetails?.name} value={name} onChange={(e) => setName(e.target.value)} />
+                                    <Button onClick={changeName}>Change Name</Button>
+                                </div>
+                                <div className="flex flex-col gap-2">
+                                    <h1>Change Password</h1>
+                                    <Input value={oldPassword} onChange={(e) => setOldPassword(e.target.value)} placeholder="Enter Old Password" />
+                                    <Input value={newPassword} onChange={(e) => setNewPassword(e.target.value)} placeholder="Enter New Password" />
+                                    <Button onClick={changePassword}>Change Password</Button>
+                                </div>
                             </div>
                         </div>
-                         </div>
                     </DialogDescription>
                 </DialogHeader>
             </DialogContent>
