9278613b647a6e91a3a5735e73f17c5967275f0d
Hitesh Navare <navarehitesh65@gmail.com>
2026-01-31 10:49:38 +0530

    added the joinRoom Requeset to backend to joinRoom on roomName in dialog

--- frontend/src/components/dashboard/JoinRoomDialog.tsx
+++ frontend/src/components/dashboard/JoinRoomDialog.tsx
@@ -9,7 +9,9 @@ import {
 } from "../ui/dialog"
 import { Input } from "../ui/input";
 import { ArrowBigRight } from "lucide-react";
-import { useSocket } from "../../config/socket";
+import axios from "axios";
+import { useRoom } from "../../config/context";
+import { toast } from "sonner";
 
 interface DialogInterface {
     isOpen: boolean;
@@ -17,15 +19,31 @@ interface DialogInterface {
 }
 
 export default function JoinRoomDialog({ isOpen, setIsOpen }: DialogInterface) {
-    const { ws } = useSocket();
-    const [roomName, setRoomName] = useState("");
+    const [name, setName] = useState("");
+    const { setIsSelected, setRoomName, setRoomId } = useRoom();
 
-    const sendMessage = async () => {
-        console.log("joinRoom", roomName, ws);
-        ws?.send(JSON.stringify({
-            "type": "JOIN_ROOM",
-            "roomId": roomName
-        }))
+    const requestJoinRoom = async () => {
+        try {
+            const response = await axios.patch(`${import.meta.env.VITE_BACKEND_URL}/room/joinRoom`, { roomName: name }, {
+                headers: {
+                    "Authorization": localStorage.getItem("token")
+                }
+            })
+
+            const data = response.data.data;
+            const { roomId, roomName } = data;
+            
+            if (response.data) {
+                setIsSelected(true);
+                setRoomName(roomName);
+                setRoomId(roomId);
+            } else {
+                toast(response.data.data.error);
+            }
+        } catch (err) {
+            console.log(err);
+        }
+        setIsOpen(false);
     }
 
     return (
@@ -34,10 +52,10 @@ export default function JoinRoomDialog({ isOpen, setIsOpen }: DialogInterface) {
                 <DialogHeader>
                     <DialogTitle>Join a Room</DialogTitle>
                     <DialogDescription>
-                        <Input value={roomName} onChange={(e) => setRoomName(e.target.value)} className="text-semibold font-medium" placeholder="Enter a Room Name" />
+                        <Input value={name} onChange={(e) => setName(e.target.value)} className="text-semibold font-medium" placeholder="Enter a Room Name" />
                         <span className="gap-4 flex mt-3 w-full justify-end">
                             <Button onClick={() => setIsOpen(false)}>Cancel</Button>
-                            <Button onClick={sendMessage}>Join Room<ArrowBigRight /></Button>
+                            <Button onClick={requestJoinRoom}>Join Room<ArrowBigRight /></Button>
                         </span>
                     </DialogDescription>
                 </DialogHeader>
