8269099536adb9d4b477b6a13675cf5a86f51327
Hitesh Navare <navarehitesh65@gmail.com>
2026-01-31 10:48:46 +0530

    added the joinRoom patch request to join the users

--- Backend/src/routes/roomRoute.ts
+++ Backend/src/routes/roomRoute.ts
@@ -1,7 +1,9 @@
 import express from "express"
 import { authMiddleware } from "../middleware/auth.js";
-import { chatModel, roomModel } from "../db/db.js";
+import { chatModel, roomModel, userModel } from "../db/db.js";
 import { createRoomSchema } from "../utils/roomTypes.js";
+import mongoose from "mongoose";
+import { check } from "zod";
 
 export const roomRouter = express.Router();
 
@@ -24,7 +26,7 @@ roomRouter.post('/create', authMiddleware, async (req, res) => {
         roomName: roomName
     })
 
-    if(checkUniqueRoomName){
+    if (checkUniqueRoomName) {
         return res.status(404).json({
             "error": false,
             "data": "Room name Should be unique"
@@ -52,20 +54,72 @@ roomRouter.post('/create', authMiddleware, async (req, res) => {
     })
 })
 
-roomRouter.get('/getRooms', authMiddleware, async(req, res) => {
+roomRouter.patch('/joinRoom', authMiddleware, async (req, res) => {
+    const userId = req.id;
+    const parsedData = createRoomSchema.safeParse(req.body);
+
+    if (!parsedData.success) {
+        return res.status(400).json({
+            "success": false,
+            "error": "not a valid roomName"
+        })
+    }
+
+    const checkRoom = await roomModel.findOne({
+        roomName: parsedData.data.roomName
+    })
+
+    if(!checkRoom){
+        return res.status(400).json({
+            "success": false,
+            "error": "Room Not Found"
+        })
+    }
+
+    if(checkRoom.users.find(x => x.toString() === userId )){
+        return res.status(201).json({
+            "success": true,
+            "data": "User Already Joined"
+        })
+    }
+
+    const joinRoom = await roomModel.updateOne({
+        _id: checkRoom._id
+    },{
+        users: userId,
+        roomName: checkRoom.roomName,
+    })
+
+    if(!joinRoom){
+        return res.status(400).json({
+            "success": false,
+            "error": "Room Not Joined"
+        })
+    }
+
+    return res.status(201).json({
+            "success": true,
+            "data": {
+                "roomId": checkRoom._id,
+                "roomName": checkRoom.roomName
+            }
+    })
+})
+
+roomRouter.get('/getRooms', authMiddleware, async (req, res) => {
     const userId = req.id;
 
     const rooms = await roomModel.find({
-        users: userId
+        users: userId,
     }).select("_id roomName")
 
     console.log("rooms", rooms);
-     if(!rooms){
+    if (!rooms) {
         return res.status(401).json({
             "success": "false",
             "error": "failed to find the room or chat"
         })
-    } 
+    }
 
     return res.status(201).json({
         "success": "true",
@@ -73,15 +127,15 @@ roomRouter.get('/getRooms', authMiddleware, async(req, res) => {
     })
 })
 
-roomRouter.get('/chats/:roomId', authMiddleware, async (req, res) => {
-    const roomId = req.params.roomId as string;
+roomRouter.get('/chats', authMiddleware, async (req, res) => {
+    const roomId = req.query.roomId as string;
     console.log("roomId", roomId);
 
     const messages = await chatModel.find({
         room: roomId
-    }).populate("user", "name email").limit(100);
-    
-    if(!messages){
+    }).populate("user", "name email profileUrl").limit(50);
+
+    if (!messages) {
         return res.status(401).json({
             "success": "false",
             "error": "failed to find the room or chat"
